31047e18a02aa2a720c3bf229d8b4c155a417dd7
Guilherme Lattmann <guilhermelattmann@hotmail.com>
2024-10-08 21:34:48 -0300

    Ajustes comandos e teclas

---
+++ script.js
@@ -0,0 +1,16 @@
+const boxDigit1 = document.getElementById ("digit1")
+const boxDigit2 = document.getElementById ("digit2")
+const boxDigit3 = document.getElementById ("digit3")
+const boxDigit4 = document.getElementById ("digit4")
+const boxDigit5 = document.getElementById ("digit5")
+
+const buttoNumber1 = document.getElementById ("number1");
+const buttoNumber2 = document.getElementById ("number2");
+const buttoNumber3 = document.getElementById ("number3");
+const buttoNumber4 = document.getElementById ("number4");
+const buttoNumber5 = document.getElementById ("number5");
+const buttoNumber6 = document.getElementById ("number6");
+const buttoNumber7 = document.getElementById ("number7");
+const buttoNumber8 = document.getElementById ("number8");
+const buttoNumber9 = document.getElementById ("number9");
+const buttoNumber0 = document.getElementById ("number0git ");
